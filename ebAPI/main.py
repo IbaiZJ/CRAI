f0b03edc626b993ad8712f5b850033db84ae015b
IbaiZJ <IIbaIIZJ@gmail.com>
2025-11-26 09:32:49 +0100

    feat: implement main application logic with FastAPI and command-line argument for data unzipping

---
+++ ebAPI/main.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+import argparse
+from routers.router import router
+from conf.config import settings
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--unzipData", action="store_true", help="Unzip and process data files")
+args = parser.parse_args()
+
+if args.unzipData:
+    print("Unzipping data files...")
+    
+
+app = FastAPI(
+    title=settings.API_TITLE,
+    version=settings.API_VERSION,
+)
+
+app.include_router(router)
