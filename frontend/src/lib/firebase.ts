2178f9e7308597abfc5145e453eb6a7d220bf5b7
IbaiZJ <IIbaIIZJ@gmail.com>
2025-11-30 11:57:08 +0100

    feat: add Firebase initialization and service exports

--- frontend/src/lib/firebase.ts
+++ frontend/src/lib/firebase.ts
@@ -14,7 +14,7 @@ const firebaseConfig = {
 };
 
 // Export the initialized Firebase services
-export const app = initializeApp(firebaseConfig);;
+export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 export const storage = getStorage(app);
