112bbfd4defcfa2b01b10a48bdefecc19d5788c3
aortizleg <aortizlegarreta@gmail.com>
2025-12-10 11:38:28 +0100

    Cambios para teses del main

--- os/src/test/java/com/crai/os/OsApplicationTests.java
+++ os/src/test/java/com/crai/os/OsApplicationTests.java
@@ -1,9 +1,9 @@
 package com.crai.os;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
@@ -15,12 +15,14 @@ class OsApplicationTests {
 	}
 
 	@Test
-	void mainStartsApplicationContext() {
-		System.setProperty("server.port", "0"); // avoid port conflicts when starting the app
-		try (var context = SpringApplication.run(OsApplication.class, new String[] {})) {
-			assertTrue(context.isActive());
+	void mainStartsAndStoresContext() {
+		String[] args = { "--spring.main.web-application-type=none", "--server.port=0", "--spring.main.banner-mode=off" };
+		try {
+			OsApplication.main(args);
+			assertNotNull(OsApplication.getContext());
+			assertTrue(OsApplication.getContext().isActive());
 		} finally {
-			System.clearProperty("server.port");
+			OsApplication.stop();
 		}
 	}
 
