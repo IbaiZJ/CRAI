a21e86996bf1a0ee55012ec138c6e06cbfc06ac9
aortizleg <aortizlegarreta@gmail.com>
2025-12-10 11:30:49 +0100

    Teses del OsAplicationTest

--- os/src/test/java/com/crai/os/OsApplicationTests.java
+++ os/src/test/java/com/crai/os/OsApplicationTests.java
@@ -1,13 +1,27 @@
 package com.crai.os;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
 class OsApplicationTests {
 
 	@Test
 	void contextLoads() {
+		// context bootstraps via @SpringBootTest
+	}
+
+	@Test
+	void mainStartsApplicationContext() {
+		System.setProperty("server.port", "0"); // avoid port conflicts when starting the app
+		try (var context = SpringApplication.run(OsApplication.class, new String[] {})) {
+			assertTrue(context.isActive());
+		} finally {
+			System.clearProperty("server.port");
+		}
 	}
 
 }
