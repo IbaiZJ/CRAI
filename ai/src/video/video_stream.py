d72885fa281b71f646e6f5bca5c533214a3521ef
IbaiZJ <IIbaIIZJ@gmail.com>
2026-01-05 23:35:38 +0100

    Enhance VideoStream and WebcamVideoStream classes with logging and resolution/framerate settings

--- ai/src/video/video_stream.py
+++ ai/src/video/video_stream.py
@@ -1,14 +1,18 @@
 from .webcam_video_stream import WebcamVideoStream
+from utils.logger import get_logger
 
 class VideoStream:
     """ Video Stream class """
     
     def __init__(self, src: int = 0, use_pi_camera: bool = False, resolution: tuple = (320, 240), framerate: int = 32):
+        self.logger = get_logger("VideoStream")
         if use_pi_camera:
             from .pi_video_stream import PiVideoStream
+            self.logger.info(f"Inicializando Pi Camera con resolution={resolution}, framerate={framerate}")
             self.stream = PiVideoStream(resolution = resolution, framerate = framerate)
         else:
-            self.stream = WebcamVideoStream(src=src)
+            self.logger.info(f"Inicializando webcam en source={src}")
+            self.stream = WebcamVideoStream(src=src, resolution=resolution, framerate=framerate)
             
     def start(self):
         # start the threaded video stream
--- ai/src/video/webcam_video_stream.py
+++ ai/src/video/webcam_video_stream.py
@@ -4,10 +4,20 @@ import cv2
 class WebcamVideoStream:
     """ WebcamVideoStream class """
     
-    def __init__(self, src:int = 0):
+    def __init__(self, src:int = 0, resolution: tuple = (640, 480), framerate: int = 30):
         # initialize the video camera stream and read the first frame
 		# from the stream
         self.stream: cv2.VideoCapture = cv2.VideoCapture(src)
+        
+        # set resolution if provided
+        if resolution is not None:
+            self.stream.set(cv2.CAP_PROP_FRAME_WIDTH, resolution[0])
+            self.stream.set(cv2.CAP_PROP_FRAME_HEIGHT, resolution[1])
+        
+        # set framerate if provided
+        if framerate is not None:
+            self.stream.set(cv2.CAP_PROP_FPS, framerate)
+        
         (self.grabbed, self.frame) = self.stream.read()
 		# initialize the variable used to indicate if the thread should
 		# be stopped
