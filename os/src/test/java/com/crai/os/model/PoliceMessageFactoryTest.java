c5577232521cec66f41831369d6e82f3f9b3c64d
aitor <aitor.murguzurl@alumni.mondragon.edu>
2026-01-07 10:08:27 +0100

    test: enhance unit tests for PoliceController, PoliceMessageFactory, OwnerRepository, and PoliceService

--- os/src/test/java/com/crai/os/model/PoliceMessageFactoryTest.java
+++ os/src/test/java/com/crai/os/model/PoliceMessageFactoryTest.java
@@ -19,14 +19,19 @@ class PoliceMessageFactoryTest {
     void buildsBadgeMessage() {
         PoliceMessage msg = PoliceMessageFactory.build(AlertType.BADGE, "1234BCD", "B");
         assertThat(msg.getType()).isEqualTo(AlertType.BADGE);
+        assertThat(msg.getPlate()).isEqualTo("1234BCD");
         assertThat(msg.getDescription()).contains("invalid environmental badge");
+        assertThat(msg.getDescription()).contains("B");
+        assertThat(msg.getRecipientEmail()).isNull();
     }
 
     @Test
     void buildsPoliceMessage() {
         PoliceMessage msg = PoliceMessageFactory.build(AlertType.POLICE, "9999ZZZ", "robado");
         assertThat(msg.getType()).isEqualTo(AlertType.POLICE);
+        assertThat(msg.getPlate()).isEqualTo("9999ZZZ");
         assertThat(msg.getDescription()).contains("stolen or wanted");
+        assertThat(msg.getRecipientEmail()).isNull();
     }
 
     @Test
@@ -35,17 +40,33 @@ class PoliceMessageFactoryTest {
         PoliceMessage soon = PoliceMessageFactory.build(AlertType.ITV, "2222BBB", "EXPIRING_SOON");
         PoliceMessage unknown = PoliceMessageFactory.build(AlertType.ITV, "3333CCC", "UNKNOWN");
 
+        assertThat(expired.getType()).isEqualTo(AlertType.ITV);
+        assertThat(expired.getPlate()).isEqualTo("1111AAA");
         assertThat(expired.getDescription()).contains("ITV expired");
-        assertThat(soon.getDescription()).contains("ITV expiring soon");
-        assertThat(unknown.getDescription()).contains("ITV could not be verified");
+        assertThat(expired.getDescription()).contains("1111AAA");
         assertThat(expired.getRecipientEmail()).isEqualTo("mail@test.com");
+
+        assertThat(soon.getType()).isEqualTo(AlertType.ITV);
+        assertThat(soon.getPlate()).isEqualTo("2222BBB");
+        assertThat(soon.getDescription()).contains("ITV expiring soon");
+        assertThat(soon.getDescription()).contains("2222BBB");
+        assertThat(soon.getRecipientEmail()).isNull();
+
+        assertThat(unknown.getType()).isEqualTo(AlertType.ITV);
+        assertThat(unknown.getPlate()).isEqualTo("3333CCC");
+        assertThat(unknown.getDescription()).contains("ITV could not be verified");
+        assertThat(unknown.getDescription()).contains("3333CCC");
+        assertThat(unknown.getRecipientEmail()).isNull();
     }
 
     @Test
     void buildsItvMessageWithDefaultCase() {
         // Test the default case in buildItvMessage switch
         PoliceMessage msg = PoliceMessageFactory.build(AlertType.ITV, "4444DDD", "INVALID_STATUS");
+        assertThat(msg.getType()).isEqualTo(AlertType.ITV);
+        assertThat(msg.getPlate()).isEqualTo("4444DDD");
         assertThat(msg.getDescription()).contains("Unknown ITV status for vehicle 4444DDD");
+        assertThat(msg.getRecipientEmail()).isNull();
     }
 
     @Test
