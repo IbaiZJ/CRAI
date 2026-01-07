cb6d03d393680750a29849384fe0c95f29f72b52
Aitor Ortiz <aortiz@ulmahandling.com>
2026-01-07 13:57:14 +0100

    Remove unused classes and beans from the project

--- os/src/main/java/com/crai/os/config/AppConfig.java
+++ os/src/main/java/com/crai/os/config/AppConfig.java
@@ -3,19 +3,9 @@ package com.crai.os.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import com.crai.os.utils.LoggingUtils;
-import com.crai.os.utils.VehicleMapper;
 
 @Configuration
 public class AppConfig {
 
-    @Bean
-    public LoggingUtils loggingUtils() {
-        return new LoggingUtils();
-    }
-
-    @Bean
-    public VehicleMapper vehicleMapper() {
-        return new VehicleMapper();
-    }
+    // No custom beans needed for now.
 }
