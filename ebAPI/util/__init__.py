de6ddfca4041e6bdaf7a66014acee7d45a64f66f
aitor <aitor.murguzurl@alumni.mondragon.edu>
2026-01-07 11:00:02 +0100

    Add docstring to __init__.py files

--- ebAPI/util/__init__.py
+++ ebAPI/util/__init__.py
@@ -1,1 +1,3 @@
+"""Utility package for ebAPI"""
 # This file makes the util directory a Python package
+
--- itvAPI/util/__init__.py
+++ itvAPI/util/__init__.py
@@ -1,1 +1,3 @@
+"""Utility package for itvAPI"""
 # This file makes the util directory a Python package
+
