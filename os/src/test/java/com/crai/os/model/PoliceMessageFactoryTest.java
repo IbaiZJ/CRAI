5bd84229f2f3c23165e3ea8625cad0099d0088ce
aitor <aitor.murguzurl@alumni.mondragon.edu>
2026-01-07 09:53:28 +0100

    fix: update message descriptions in PoliceMessageFactoryTest for clarity and consistency

--- os/src/test/java/com/crai/os/model/PoliceMessageFactoryTest.java
+++ os/src/test/java/com/crai/os/model/PoliceMessageFactoryTest.java
@@ -19,14 +19,14 @@ class PoliceMessageFactoryTest {
     void buildsBadgeMessage() {
         PoliceMessage msg = PoliceMessageFactory.build(AlertType.BADGE, "1234BCD", "B");
         assertThat(msg.getType()).isEqualTo(AlertType.BADGE);
-        assertThat(msg.getDescription()).contains("etiqueta NO valida");
+        assertThat(msg.getDescription()).contains("invalid environmental badge");
     }
 
     @Test
     void buildsPoliceMessage() {
         PoliceMessage msg = PoliceMessageFactory.build(AlertType.POLICE, "9999ZZZ", "robado");
         assertThat(msg.getType()).isEqualTo(AlertType.POLICE);
-        assertThat(msg.getDescription()).contains("ROBADO");
+        assertThat(msg.getDescription()).contains("stolen or wanted");
     }
 
     @Test
@@ -35,9 +35,9 @@ class PoliceMessageFactoryTest {
         PoliceMessage soon = PoliceMessageFactory.build(AlertType.ITV, "2222BBB", "EXPIRING_SOON");
         PoliceMessage unknown = PoliceMessageFactory.build(AlertType.ITV, "3333CCC", "UNKNOWN");
 
-        assertThat(expired.getDescription()).contains("CADUCADA");
-        assertThat(soon.getDescription()).contains("proxima a caducar");
-        assertThat(unknown.getDescription()).contains("No se pudo verificar");
+        assertThat(expired.getDescription()).contains("ITV expired");
+        assertThat(soon.getDescription()).contains("ITV expiring soon");
+        assertThat(unknown.getDescription()).contains("ITV could not be verified");
         assertThat(expired.getRecipientEmail()).isEqualTo("mail@test.com");
     }
 
@@ -45,7 +45,7 @@ class PoliceMessageFactoryTest {
     void buildsItvMessageWithDefaultCase() {
         // Test the default case in buildItvMessage switch
         PoliceMessage msg = PoliceMessageFactory.build(AlertType.ITV, "4444DDD", "INVALID_STATUS");
-        assertThat(msg.getDescription()).contains("ITV desconocida para 4444DDD");
+        assertThat(msg.getDescription()).contains("Unknown ITV status for vehicle 4444DDD");
     }
 
     @Test
