e177d3f49124dcd325fbf1d6816ae164721479ca
IbaiZJ <IIbaIIZJ@gmail.com>
2026-01-10 23:17:59 +0100

    feat: add new vehicle and AI API tabs, refactor existing subflows and endpoints
    - Introduced new tabs for VEHICLE API and AI API in flows.json.
    - Renamed subflows for ITV and EB API requests to remove "with Retry" from their names.
    - Added a new subflow for validating vehicle plates.
    - Updated the routing logic in router.py to use Query parameters for carPlate.
    - Refactored various function nodes and HTTP endpoints for better organization and clarity.
    - Adjusted coordinates for nodes in flows.json for improved layout.

--- ebAPI/routers/router.py
+++ ebAPI/routers/router.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Query
 from conf.config import settings
 from service.service import plateService
 
@@ -8,6 +8,6 @@ router = APIRouter(
 )
 
 @router.get("")
-async def detect_plate(car_plate: str):
+async def detect_plate(car_plate: str = Query(..., alias="carPlate")):
     result: str = plateService.get_badge_by_plate(car_plate)
     return {"carPlate": car_plate, "badge": result}
