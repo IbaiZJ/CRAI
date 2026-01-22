a9cd19872c6ab284828d0b1775b4b2ffc95c71fd
aitor <aitor.murguzurl@alumni.mondragon.edu>
2026-01-22 11:02:14 +0100

    Sonarcloud error

---
+++ frontend/test-coverage-fix.cjs
@@ -0,0 +1,15 @@
+// Script para reemplazar '\\' por '/' en lcov.info tras los tests (CommonJS)
+const fs = require('fs');
+const path = require('path');
+
+const lcovPath = path.join(__dirname, 'coverage', 'lcov.info');
+if (fs.existsSync(lcovPath)) {
+  let content = fs.readFileSync(lcovPath, 'utf8');
+  // Reemplaza solo en las líneas SF:
+  content = content.replace(/^SF:(.*)\\/gm, (m) => m.replace(/\\/g, '/'));
+  fs.writeFileSync(lcovPath, content, 'utf8');
+  console.log('Rutas de lcov.info corregidas a formato Unix.');
+} else {
+  console.error('No se encontró coverage/lcov.info');
+  process.exit(1);
+}
