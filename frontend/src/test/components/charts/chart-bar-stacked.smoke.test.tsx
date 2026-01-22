f0a487f9987d78e90230e3759bb3400d55b18836
aitor <aitor.murguzurl@alumni.mondragon.edu>
2026-01-22 10:43:58 +0100

    fix: correct import statement for ChartBarStacked component in smoke test

--- frontend/src/test/components/charts/chart-bar-stacked.smoke.test.tsx
+++ frontend/src/test/components/charts/chart-bar-stacked.smoke.test.tsx
@@ -1,5 +1,5 @@
 import { render } from '@testing-library/react';
-import ChartBarStacked from '../../../components/charts/barCharts/chart-bar-stacked';
+import { ChartBarStacked } from '../../../components/charts/barCharts/chart-bar-stacked';
 describe('Smoke test ChartBarStacked', () => {
   it('renders ChartBarStacked', () => {
     render(<ChartBarStacked data={[]} />);
